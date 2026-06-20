3c0887b775fa4705a5ea0634335108db4f05ca4c
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-20 14:18:08 +0530

    check env

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,6 +5,9 @@ const apiClient = axios.create({
   withCredentials: true,
 });
 
+console.log("API URL =", import.meta.env.VITE_API_URL);
+console.log("SOCKET URL =", import.meta.env.VITE_SOCKET_URL);
+
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('accessToken');
